b665592313cc5ee1bfbf938127f1fee13b6956a1
etudiants <ifosup@ifosup.wavre.be>
2022-12-07 15:03:27 +0100

    Deuxième version

--- app.py
+++ app.py
@@ -1,14 +1,32 @@
 user_account = {}
 
 
+def login_add(user_name: str):
+    print("Enter your account data.")
+    site_name = input("What's the website or the game of your account ? ")
+    login_account = input("What's your name on this site ? ")
+    password = input("What's your password ? ")
+    user_account[user_name] = [site_name, login_account, password]
+
+
 def login():
     user_name = input("What's your name ?")
     if user_name in user_account.keys():
-        print("Enter your account.")
-        site_name = input("")
-        login = input()
-        password = input()
-        user_account[user_name] = [site_name,login,password]
+        print('Welcome ' + user_name + '! \n 1. Add an account \n 2. Read an account \n 3. Change an account \n 4. '
+                                       'Suppress an account \n 0. exit the login')
+        num_match = input("Enter a number")
+        while num_match != 0:
+            match num_match:
+                case 1:
+                    login_add(user_name)
+                case 2:
+                    login_read()
+                case 3:
+                    login_change()
+                case 4:
+                    login_suppress()
+
+
 
 
 def register():
@@ -30,7 +48,7 @@ def suppress():
 def main_app():
     selection: int = -1
     print("Welcome in Passman")
-    print("What can I do for you \n 1. Login \n 2. Register \n 3.update \n 4. Suppress")
+    print("What can I do for you \n 1. Login \n 2. Register \n 3.Suppress \n 0. exit")
     while selection != 0:
         selection = int(input("Enter a number : "))
         match selection:
