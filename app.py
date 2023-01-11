5b6adfb17402ab89a650d21daf38e6333f2c9a05
etudiants <ifosup@ifosup.wavre.be>
2023-01-11 13:32:46 +0100

    second added objects.

--- app.py
+++ app.py
@@ -1,38 +1,4 @@
-user_account = {}
-
-
-class SignPaper:
-
-    def __init__(self, user_name: str, password: str):
-        self.user_name = {}
-        self.password = {}
-        self.AccountVault = AccountVault(user_name)
-
-    def change(self, decision: int, used_string: str):
-        match decision:
-            case 1:
-                self.user_name = used_string
-            case 2:
-                self.password = used_string
-
-
-class AccountVault:
-
-    def __init__(self, user_name: str, user_account_site: str = 'empty', user_account_name: str = 'empty',
-                 user_account_password: str = 'empty'):
-        self.user_name: str = 'empty_string'
-        self.user_account_site = []
-        self.user_account_name = []
-        self.user_account_password = []
-
-    def change(self, decision: int, used_string: str):
-        match decision:
-            case 1:
-                self.user_account_site = used_string
-            case 2:
-                self.user_account_name = used_string
-            case 3:
-                self.user_account_password = used_string
+user_account = []
 
 
 def login_add(user_name: str):
@@ -112,8 +78,9 @@ def login():
 
 
 def register():
-    user_name = input("What's your new name ? ")
-    if user_name not in user_account.keys():
+    user_name = input("What's your name ? ")
+    user_password = input("What's your password ? ")
+    if user_name == SignPaper.user_name :
         user_account[user_name] = []
     else:
         print("This name already.")
---
+++ appobj.py
@@ -0,0 +1,32 @@
+class SignPaper:
+
+    def __init__(self, user_name: str, password: str):
+        self.user_name = {'username': user_name}
+        self.password = {'password': password}
+        self.AccountVault = AccountVault(user_name)
+
+    def change(self, decision: int, used_string: str):
+        match decision:
+            case 1:
+                self.user_name = used_string
+            case 2:
+                self.password = used_string
+
+
+class AccountVault:
+
+    def __init__(self, user_name: str, user_account_site: str = 'empty', user_account_name: str = 'empty',
+                 user_account_password: str = 'empty'):
+        self.user_name: str = 'empty_string'
+        self.user_account_site = []
+        self.user_account_name = []
+        self.user_account_password = []
+
+    def change(self, decision: int, used_string: str):
+        match decision:
+            case 1:
+                self.user_account_site = used_string
+            case 2:
+                self.user_account_name = used_string
+            case 3:
+                self.user_account_password = used_string
